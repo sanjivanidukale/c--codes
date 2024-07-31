0e21a164b6d2d8400e2b3f439dc788b111aa4420
sanjivanidukale <dukalesanjana0@gmail.com>
2024-07-31 16:14:37 +0530

    program1

---
+++ program2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float x,y,product;
+    printf("enter a number");
+    scanf("%f%f",&x,&y);
+    product=x*y;
+    printf("the product of two numbers is %f",product);
+    return 0;
+}
